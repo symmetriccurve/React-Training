aeffbe8911b62c41fa9440c0821eaca6af19d89f
Vikram Belde <belvikram@gmail.com>
2017-10-19 10:42:05 -0500

    Day 3 Class

--- client/Components/App.js
+++ client/Components/App.js
@@ -2,49 +2,37 @@ import React,{ Component} from 'react';
 //importing from React Library 
 //
 
-var data = {
-    title : "Some Title",
-    price : "$100",
-    description: "Some Description"
-}
+
+class App extends Component{
+
+    constructor(){ //1
+        super()
+        this.state = {
+            name:'Sesidhar'
+        }
+    }
+
+    componentWillMount(){ //2
+        this.setState({
+            name : "Vinay"
+        })
+    }
 
 
-class App extends Component{  // This is ES6 Way to create a Class
-    //all the life cycle methods goes here
-    // All the React Components should/ Must return a View
-
-    render(){ // One of th Life Cycle Method
+    render(){  //3 //5
         return(
-             <div>
-                <div style={{display:'flex',padding:'20px',height:'200px',width:'500px',backgroundColor:'#44B6EC'}}>
-                    <div style={{display:'flex',flex:1,flexDirection:'column'}}>
-                        <div style={{display:'flex',height:'30%'}}>
-                            <div style={{display:'flex',flex:3,height:'30%',alignItems:'center',justifyContent:'flex-start'}}>
-                                <span style={{color:'#434040',fontFamily: 'Verdana, Geneva, sans-serif',fontSize:'40px',fontWeight:'bold',color:'white'}}>
-                                    {data.title}
-                                </span>
-                            </div>
-                           <div style={{display:'flex',flex:1,height:'30%',alignItems:'center',justifyContent:'flex-start'}}>
-                               <span style={{color:'#434040',fontFamily: 'Verdana, Geneva, sans-serif',fontSize:'20px',color:'white'}}>
-                                    {data.price}
-                                </span>
-                            </div>
-                        </div>
-                        <div style={{display:'flex',height:'40%',alignItems:'center',justifyContent:'flex-start', shadowColor: '#000000',shadowOffset: {width: 0,height: 3},shadowRadius: 1,shadowOpacity: 0.1}}>
-                            <span style={{color:'#434040',fontFamily: 'Verdana, Geneva, sans-serif',fontSize:'20px',color:'white'}}>
-                                 {data.description}
-                            </span>
-                        </div>
-                        <div style={{display:'flex',backgroundColor:'white',height:'25%',alignItems:'center',justifyContent:'center', shadowColor: '#000000',shadowOffset: {width: 0,height: 3},shadowRadius: 1,shadowOpacity: 0.1}}>
-                            <span style={{color:'#434040',fontFamily: 'Verdana, Geneva, sans-serif',fontSize:'20px',fontWeight:'800'}}>
-                                Click Me
-                            </span>
-                        </div>
-                    </div>
-                </div>    
-            </div>
+            <div> 
+                <div> 
+                    {this.state.name} 
+                </div>
+            </div>   
         )
     }
+
+    componentDidmount(){
+        
+    }
+
 
 }
 module.exports = App
