8f8fe63c4a2973eacb300205e856f160329cf32b
Vikram Belde <belvikram@gmail.com>
2017-10-20 19:40:34 -0500

    BootStrap Added

--- Notes/Day 4/ProductCard.js
+++ Notes/Day 4/ProductCard.js
@@ -3,15 +3,15 @@ import React,{ Component} from 'react';
 class ProductCard extends Component{
     render(){
         return(
-            <div style={{marginTop:'10px'}}>
+            <div>
                 <div style={{display:'flex',padding:'20px',height:'200px',width:'500px',backgroundColor:'#44B6EC'}}>
                     <div style={{display:'flex',flex:1,flexDirection:'column',alignItems:'center',justifyContent:'flex-start'}}>
                         <span style={{color:'#434040',fontFamily: 'Verdana, Geneva, sans-serif',fontSize:'40px',fontWeight:'bold',color:'white'}}>
-                            {this.props.title}
+                            Title
                          </span>
-                         <div style={{marginTop:'10%',display:'flex',backgroundColor:'white',height:'25%',width:'100%',alignItems:'center',justifyContent:'center', shadowColor: '#000000',shadowOffset: {width: 0,height: 3},shadowRadius: 1,shadowOpacity: 0.1}} onClick={this.handleClick}>
+                         <div style={{marginTop:'10%',display:'flex',backgroundColor:'white',height:'25%',width:'100%',alignItems:'center',justifyContent:'center', shadowColor: '#000000',shadowOffset: {width: 0,height: 3},shadowRadius: 1,shadowOpacity: 0.1}} onClick={()=>{this.setState({name:'hello'})}}>
                             <span style={{color:'#434040',fontFamily: 'Verdana, Geneva, sans-serif',fontSize:'20px',fontWeight:'800'}}>
-                               "Button"
+                                Click Me
                             </span>
                         </div>
                     </div>    
